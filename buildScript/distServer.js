f232ec7ab7c4856a6e162145e889c0852b156c6e
jan-far <ligalifarouk@gmail.com>
2020-08-15 17:15:02 +0100

    Create distServer.js

---
+++ buildScript/distServer.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path from "path";
+import open from "open";
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+})
+
+app.get("/users", (req, res) => {
+  //Pretend this is a real database
+  res.json(
+    [
+      { 'id': 1, 'firstName': 'Bob', 'lastName': 'Smith', 'email': 'bob@gmail.com'},
+      { 'id': 1, 'firstName': 'Tanny', 'lastName': 'Norton', 'email': 'tnorton@gmail.com'},
+      { 'id': 1, 'firstName': 'Tina', 'lastName': 'Lee', 'email': 'lee.tina@gmail.com'}
+    ]
+  )
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+})

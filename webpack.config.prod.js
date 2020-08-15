5ddad8cbd0323e0fc36e64cad8367248487207ad
jan-far <ligalifarouk@gmail.com>
2020-08-15 21:06:37 +0100

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,28 +1,58 @@
 import path from "path";
 import webpack from "webpack";
-// import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
-    mode: "development",
+    mode: "production",
     // debug: true,
     devtool: "source-map",
     // noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname, 'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: "/",
-        filename: "bundle.js"
+        filename: "[name].[chunkhash].js"
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'src')
     },
     plugins: [
+      //Generate a external css file with a hash in the filename
+      new MiniCssExtractPlugin({
+        filename: '[name].[chunkhash].css'
+      }),
+
+      // Hash the file using MD5 so that their names change when the content changes
+      new WebpackMd5Hash(),
+
+      //Create Html file that includes reference to bundled js
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
+        },
+        inject: true
+      }),
+
       //Eliminate duplicate packages when generating bundle
       // webpack.optimize.DedupePlugin,
-      //Minify jsnpm install uglifyjs-webpack-plugin --save-dev
+
       new webpack.LoaderOptionsPlugin({
        debug: true
       })
@@ -30,7 +60,18 @@ export default {
     module: {
         rules: [
           {test: /\.js$/, exclude: /node_module/, loaders: ['babel-loader']},
-            {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+            {test: /\.css$/, loaders: [MiniCssExtractPlugin.loader, 'css-loader']}
           ]
+    },
+    optimization: {
+      //Use SplitChunks to create a separate bundle files of vendor libraries
+      //so that they are catched separately
+      splitChunks: {
+        // chunks: 'all',
+        // minSize: 2000
+      },
+
+      //Minify
+      minimizer: [new UglifyJSPlugin()]
     }
   }
